835ac7edb36a7e146381f5ec7dd90818cbffcc66
SimchaTeich <sim.teich@gmail.com>
2023-05-29 15:23:20 +0300

    create AscendingIterator.hpp

---
+++ sources/AscendingIterator.hpp
@@ -0,0 +1,13 @@
+#ifndef ASCENDINGITERATOR_HPP
+#define ASCENDINGITERATOR_HPP
+
+#include "MagicalContainer.hpp"
+
+using namespace ariel;
+
+class MagicalContainer::AscendingIterator
+{
+
+};
+
+#endif // !ASCENDINGITERATOR_HPP
