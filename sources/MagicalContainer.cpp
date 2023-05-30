ffbb900d1cc83b47f89f93839f4588ac835a1b90
SimchaTeich <sim.teich@gmail.com>
2023-05-30 19:46:01 +0300

    implements the C'TOR

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -1,2 +1,15 @@
 #include "MagicalContainer.hpp"
 
+using namespace ariel;
+
+MagicalContainer::MagicalContainer()
+{
+    _size = 0;
+    _primeSize = 0;
+    _head = nullptr;
+    _tail = nullptr;
+    _primeHead = nullptr;
+}
+
+
+
