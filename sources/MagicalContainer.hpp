bcc1719fd38e5f9c9d81a36dde07b0f8a011d380
SimchaTeich <sim.teich@gmail.com>
2023-05-31 01:59:27 +0300

    move headers of AscendingIterator.hpp into MagicalContainer.hpp (according to the Task instruction..)

--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -41,6 +41,28 @@ namespace ariel
         //class SideCrossIterator;
         //class PrimeIterator;
     };
-}
+
+
+    class MagicalContainer::AscendingIterator
+    {
+    private:
+        MagicalContainer& _container;
+        MisticalElementNode *_pElement;
+        int _stepsNo;
+        
+    public:
+        AscendingIterator(MagicalContainer& container);
+        //AscendingIterator(AscendingIterator& other);
+        //~AscendingIterator();
+
+        // operatores
+        AscendingIterator& operator++(); // pre
+        int operator*();
+        bool operator!=(const AscendingIterator& other);
+
+        AscendingIterator begin();
+        AscendingIterator end();
+    };
+};
 
 #endif // !MAGICALCONTAINER_HPP
