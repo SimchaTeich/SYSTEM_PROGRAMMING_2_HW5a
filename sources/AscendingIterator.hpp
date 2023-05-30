dc1ab19099720dfdd8588a1376f62e7dd175f237
SimchaTeich <sim.teich@gmail.com>
2023-05-31 00:09:02 +0300

    write some basic headers for AscendingIterator

--- sources/AscendingIterator.hpp
+++ sources/AscendingIterator.hpp
@@ -8,11 +8,21 @@ using namespace ariel;
 class MagicalContainer::AscendingIterator
 {
 private:
-    int step;
-    int *pelement;
+    MagicalContainer& _container;
+    MisticalElementNode *_pElement;
+    int stepsNo;
     
 public:
-    AscendingIterator(MagicalContainer &container);
+    AscendingIterator(MagicalContainer& container);
+    AscendingIterator(AscendingIterator& other);
+    ~AscendingIterator();
+
+    // operatores
+    friend AscendingIterator& operator++(AscendingIterator& iterator); // pre
+    friend int operator*(AscendingIterator& iterator);
+
+    AscendingIterator begin();
+    AscendingIterator end();
 };
 
 #endif // !ASCENDINGITERATOR_HPP
