7006818ad03f10c965194e8d32c9f99874c2b107
SimchaTeich <sim.teich@gmail.com>
2023-05-30 19:39:55 +0300

    wrire the headers

--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -1,9 +1,40 @@
 #ifndef MAGICALCONTAINER_HPP
 #define MAGICALCONTAINER_HPP
 
-class MagicalContainer
+#include "MisticalElementNode.hpp"
+
+namespace ariel
 {
+    class MagicalContainer
+    {
+    public:
+        MagicalContainer();
+        void addElement(int value);
+        void removeElement(int value);
+        void size() const;
+
+    private:
+        // helpers methods
+        bool isPrime(int num) const;
+        MisticalElementNode *createNode(int value) const;
+        void insertNode(MisticalElementNode *pNode);
+        void printLinkedList(MisticalElementNode *pHead) const;
+
+        // members
+        MisticalElementNode *_head;
+        MisticalElementNode *_tail;
+        MisticalElementNode *_primeHead;
+        MisticalElementNode *_primeTail;
+        int _size;
+        int _primeSize;
     
-};
+    // code for Iterators in extrenal hpp files.
+    // https://stackoverflow.com/questions/28176371/how-can-i-avoid-this-ugly-nested-class-c-code
+    public:
+        //class AscendingIterator;
+        //class SideCrossIterator;
+        //class PrimeIterator;
+    };
+}
 
 #endif // !MAGICALCONTAINER_HPP
