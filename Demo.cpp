e6c7a123186b070df0ae0a7374c836c9be374655
SimchaTeich <sim.teich@gmail.com>
2023-05-31 13:12:27 +0300

    all demo is working now

--- Demo.cpp
+++ Demo.cpp
@@ -22,21 +22,21 @@ int main() {
     }
     std::cout << std::endl;
 
-    // // Use DescendingIterator to display elements in descending order
-    // std::cout << "Elements in cross order:\n";
-    // MagicalContainer::SideCrossIterator crossIter(container);
-    // for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
-    //     std::cout << *it << ' ';  // 2 25 3 17 9
-    // }
-    // std::cout << std::endl;
+    // Use DescendingIterator to display elements in descending order
+    std::cout << "Elements in cross order:\n";
+    MagicalContainer::SideCrossIterator crossIter(container);
+    for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
+        std::cout << *it << ' ';  // 2 25 3 17 9
+    }
+    std::cout << std::endl;
 
-    // // Use PrimeIterator to display prime numbers only
-    // std::cout << "Prime numbers:\n";
-    // MagicalContainer::PrimeIterator primeIter(container);
-    // for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
-    //     std::cout << *it << ' ';  // 2 3 17 
-    // }
-    // std::cout << std::endl;
+    // Use PrimeIterator to display prime numbers only
+    std::cout << "Prime numbers:\n";
+    MagicalContainer::PrimeIterator primeIter(container);
+    for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+        std::cout << *it << ' ';  // 2 3 17 
+    }
+    std::cout << std::endl;
 
     // Remove an element from the container and display the size
     container.removeElement(9);
